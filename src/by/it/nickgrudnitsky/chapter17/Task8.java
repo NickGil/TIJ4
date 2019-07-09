9d8f26d0def12a69c9c7d69f9e87f3e1d527d321
Nick Grudnitsky <grud97@gmail.com>
2019-07-09 15:03:33 +0300

    Singly linked list

--- src/by/it/nickgrudnitsky/chapter17/Task8.java
+++ src/by/it/nickgrudnitsky/chapter17/Task8.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
+
 class SList<T> {
     private int size = 0;
     private Cell<T> current;
@@ -31,6 +32,10 @@ class SList<T> {
     }
 
 
+    SListIterator<T> iterator() {
+        return new SListIterator<>(first);
+    }
+
     private class Cell<C> {
         Cell<C> next = null;
         C type;
@@ -46,26 +51,73 @@ class SList<T> {
         }
     }
 
+    public class SListIterator<C> {
+        int cursor = 0;
+        private Cell<C> current;
+
+        SListIterator(Cell<C> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            if (cursor++ == 0) {
+                return current != null;
+            }
+            return current != null;
+        }
+
+        public Cell<C> next() {
+            Cell<C> buffer = current;
+            current = current.next;
+            return buffer;
+        }
+
+        public void remove() {
+            if(current.next != null) {
+                current.next = current.next.next;
+            }
+        }
+
+        void insert(C inserted) {
+            for (int i = 0; i < size-1; i++) {
+                current = current.next;
+            }
+            current.next = new Cell<>(inserted);
+            size++;
+        }
+
+        void insert(C inserted, int posotion) {
+            current = (Cell)first;
+            Cell<C> newCell = new Cell<>(inserted);
+            if (posotion == 1) {
+                newCell.next = current;
+                first = (Cell<T>) newCell;
+                size++;
+                return;
+            }
+            for (int i = 0; i < posotion-2; i++) {
+                current = current.next;
+            }
+            newCell.next = current.next;
+            current.next = newCell;
+            size++;
+        }
+    }
+
+
     @Override
     public String toString() {
-        if (first == null ){
+        if (first == null) {
             return "[]";
         }
         StringBuilder sb = new StringBuilder();
-        Cell<T> next = first.next;
+        SListIterator<T> iterator = iterator();
         sb.append("[");
-        for (int i = 0; i < size; i++) {
-            if (i == 0) {
-                sb.append(first.toString()).append(", ");
-                continue;
-            }
-            if (i == size - 1) {
-                sb.append(next.toString());
-                continue;
-            }
-            sb.append(next.toString()).append(", ");
-            next = next.next;
+        while (iterator.hasNext()) {
+            sb.append(iterator.next()).append(iterator.hasNext() ? ", " : "" );
+
         }
+        sb.substring(2);
         sb.append("]");
         return sb.toString();
     }
@@ -73,11 +125,20 @@ class SList<T> {
 
 public class Task8 {
     public static void main(String[] args) {
-        SList<Integer> list = new SList<>(Arrays.asList(1,2,3));
+        SList<Integer> list = new SList<>(Arrays.asList(1, 2, 3));
         SList<Integer> list2 = new SList<>(Collections.singletonList(1));
         SList<Integer> list3 = new SList<>();
         System.out.println(list);
         System.out.println(list2);
         System.out.println(list3);
+        SList<Integer>.SListIterator<Integer> iterator = list.iterator();
+        iterator.insert(4);
+        System.out.println(list);
+        iterator.insert(5,1);
+        System.out.println(list);
+        iterator.insert(6,6);
+        System.out.println(list);
+        iterator.remove();
+        System.out.println(list);
     }
 }
